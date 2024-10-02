c96a3f0d6431653f742a3193b8ed0de3972845fb
Lorae <stojanovic.lorae@gmail.com>
2024-10-02 11:35:33 -0400

    docs(map_geographies, map_data): add Roxygen2 documentation

--- R/visualization.R
+++ R/visualization.R
@@ -1,18 +1,46 @@
-# Creates a simple black and white map of shapefiles. Useful for visualizing the number
-# of CPUMAs in a region. Cannot graph data other than simple shapefiles.
+#' Create a Black and White Map of Shapefiles
+#'
+#' This function generates a simple black and white map using shapefiles. It is useful for visualizing 
+#' the number of CPUMAs or other geographic boundaries in a region. The function cannot graph data other 
+#' than simple shapefiles.
+#'
+#' @param data A `sf` object (simple feature) containing the shapefile data to be plotted.
+#'
+#' @return A `ggplot2` plot object displaying a black and white map of the shapefile.
+#' @examples
+#' # Example usage:
+#' # Assuming `sf_data` is a simple feature (sf) object
+#' map_geographies(sf_data)
+#'
+#' @export
 map_geographies <- function(data) {
   ggplot(data) +
     geom_sf(fill = "white", color = "black") +
     theme_void()
 }
 
-# Creates a colored map of shapefiles, with the color indicating the gradient of the
-# data. Data must contain a column called `diff` for this function to work as intended.
+
+#' Create a Colored Map of Shapefiles with Gradient
+#'
+#' This function generates a colored map using shapefiles, where the color indicates the gradient of the data. 
+#' The data must contain a column named `diff` for the function to work as intended. Optionally, the function 
+#' can add borders to the shapes.
+#'
+#' @param data A `sf` object (simple feature) containing the shapefile data to be plotted. It must contain 
+#'   a column called `diff` to represent the gradient.
+#' @param borders A logical indicating whether to include black borders around the shapes. Defaults to `TRUE`.
+#'
+#' @return A `ggplot2` plot object displaying a colored map of the shapefile with gradient fill based on the `diff` column.
+#' @examples
+#' # Example usage:
+#' # Assuming `sf_data` is a simple feature (sf) object with a `diff` column
+#' map_data(sf_data, borders = TRUE)
+#'
+#' @export
 map_data <- function(
     data,
     borders = TRUE
 ) {
-  
   # If borders are turned on, assign this setting to black
   border_color <- if (borders) "black" else NA
   
