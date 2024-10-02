4935bfefbd6f87c36a1d172207fe4c0aceef1ba3
Lorae <stojanovic.lorae@gmail.com>
2024-10-02 11:37:34 -0400

    docs(validate_row_counts): add Roxygen2 documentation

--- R/validation.R
+++ R/validation.R
@@ -1,11 +1,23 @@
-# Stops pipeline execution of rows are unintentionally dropped.
-# Note: input data must be a database
+#' Validate Row Counts in a Database
+#'
+#' This function checks whether the actual number of rows in a database matches the expected number of rows. 
+#' If the actual count does not match the expected count, the function stops the pipeline and raises an error. 
+#' It is typically used to ensure data integrity during data processing steps.
+#'
+#' @param db A database connection or a lazy database reference representing the data being validated.
+#' @param expected_count An integer representing the expected number of rows in the database.
+#' @param step_description A string providing a description of the pipeline step being validated, which will 
+#'   be included in the error message if validation fails.
+#'
+#' @return This function does not return a value. If the validation fails (i.e., the actual row count does 
+#'   not match the expected count), it raises an error and stops the pipeline execution.
+#'
+#' @export
 validate_row_counts <- function(
-    db, # A database of the data being validated
-    expected_count,# The expected number of rows
+    db, 
+    expected_count,
     step_description
 ) {
-  
   actual_count <- db |>
     summarize(count = n()) |>
     pull(count)
@@ -14,7 +26,8 @@ validate_row_counts <- function(
     stop(glue::glue(
       "DATA VALIDATION FAILED: Unexpected number of rows found after ",
       "{step_description}: expected {expected_count}, got {actual_count}"
-      ))
+    ))
   }
 }
+
 
