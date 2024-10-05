a2b357760d3110fecfb45e94c97806f1914c6298
Lorae <stojanovic.lorae@gmail.com>
2024-10-05 18:17:34 -0400

    feat(create_sql_query): check if lookup table has 0 rows
    If so, output a SQL query that produces a new row populated with NAs.

--- R/beta.R
+++ R/beta.R
@@ -1,36 +1,47 @@
 create_sql_query <- function(
     lookup_table  # This is the input tibble or data frame
 ) {
-  # Create the SQL query dynamically based on the input lookup table
-  lookup_sql <- paste0(
-    "WITH age_buckets AS (\n    ",
-    paste(
-      apply(lookup_table, 1, function(row) {
-        # Handle infinite upper bound by using NULL in SQL
-        upper_bound <- ifelse(is.infinite(row["upper_bound"]), "NULL", row["upper_bound"])
-        paste0("SELECT '", row["bucket_name"], "' AS bucket_name, ", 
-               row["lower_bound"], " AS lower_bound, ", upper_bound, " AS upper_bound")
-      }), 
-      collapse = "\n    UNION ALL\n    "
-    ),
-    "\n)\n"
-  )
-  
-  # Build the final SQL query using the dynamically generated WITH clause
-  query <- paste0(
-    lookup_sql,
-    "-- Applying the lookup table to the AGE column\n",
-    "SELECT \n",
-    "    data.*, \n",
-    "    COALESCE(bucket_name, 'Unknown') AS AGE_bucketed\n",
-    "FROM \n",
-    "    ipums_bucketed AS data\n",
-    "LEFT JOIN \n",
-    "    age_buckets\n",
-    "ON \n",
-    "    data.AGE >= age_buckets.lower_bound \n",
-    "    AND (data.AGE < age_buckets.upper_bound OR age_buckets.upper_bound IS NULL);"
-  )
+  # If there are no range-based lookup rules...
+  if (nrow(lookup_table) == 0) {
+    # Generate SQL query that appends an AGE_bucketed column filled with NAs
+    query <- "
+SELECT 
+    data.*, 
+    NULL AS AGE_bucketed 
+FROM 
+    ipums_bucketed AS data;"
+  } else {
+    # Create the SQL query dynamically based on the input lookup table
+    lookup_sql <- paste0(
+      "WITH age_buckets AS (\n    ",
+      paste(
+        apply(lookup_table, 1, function(row) {
+          # Handle infinite upper bound by using NULL in SQL
+          upper_bound <- ifelse(is.infinite(row["upper_bound"]), "NULL", row["upper_bound"])
+          paste0("SELECT '", row["bucket_name"], "' AS bucket_name, ", 
+                 row["lower_bound"], " AS lower_bound, ", upper_bound, " AS upper_bound")
+        }), 
+        collapse = "\n    UNION ALL\n    "
+      ),
+      "\n)\n"
+    )
+    
+    # Build the final SQL query using the dynamically generated WITH clause
+    query <- paste0(
+      lookup_sql,
+      "-- Applying the lookup table to the AGE column\n",
+      "SELECT\n",
+      "    data.*,\n",
+      "    COALESCE(bucket_name, 'Unknown') AS AGE_bucketed\n",
+      "FROM\n",
+      "    ipums_bucketed AS data\n",
+      "LEFT JOIN\n",
+      "    age_buckets\n",
+      "ON\n",
+      "    data.AGE >= age_buckets.lower_bound\n",
+      "    AND (data.AGE < age_buckets.upper_bound OR age_buckets.upper_bound IS NULL);"
+    )
+  }
   
   return(query)
 }
