f0ce4c2697b7ca59a56740a7e24b5242b5af1be6
Lorae <stojanovic.lorae@gmail.com>
2024-10-06 12:43:55 -0400

    feat: add create_range_logic function for generating SQL CASE WHEN logic based on range lookup table

--- R/beta.R
+++ R/beta.R
@@ -30,6 +30,41 @@ create_value_logic <- function(
   # Combine the fragments into a single string
   paste(unlist(sql_fragments), collapse = "\n")
 }
+
+
+
+  
+create_range_logic <- function(
+  range_lookup_table,
+  col
+) {
+  # Define a function that writes each line of output
+  write_sql_fragment <- function(i) {
+    # Read values from lookup table needed to write one line of the query
+    bucket_name <- range_lookup_table$bucket_name[i]
+    lower_bound <- range_lookup_table$lower_bound[i]
+    upper_bound <- range_lookup_table$upper_bound[i]
+
+    fragment <- glue(
+      "WHEN {col} >= {lower_bound} AND {col} < {upper_bound} THEN '{bucket_name}'"
+      )
+    
+    return(fragment)
+  }
+  
+  # Use write_sql_fragment() to write one line of SQL for each row of the 
+  # lookup table
+  sql_fragments <- lapply(
+    seq_len(nrow(range_lookup_table)),
+    write_sql_fragment
+  )
+  
+  # Combine the fragments into a single string
+  paste(unlist(sql_fragments), collapse = "\n")
+  
+}
+
+
 create_sql_query <- function(
     lookup_table,
     col # Must be a string
