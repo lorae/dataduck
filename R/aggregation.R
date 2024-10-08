6fdc30cea22f6f99f9370e7dea48e1e63b2ed483
Lorae <stojanovic.lorae@gmail.com>
2024-10-07 21:10:02 -0400

    feat(create_crosstabs): define function dynamically creating crosstabs

--- R/aggregation.R
+++ R/aggregation.R
@@ -43,7 +43,29 @@ weighted_mean <- function(
     select(!!!group_by_cols, count, sum_weights, weighted_mean)
 }
 
-
+create_crosstabs <- function(
+    data, 
+    weight_column, 
+    group_by_columns) {
+  
+  # Use quasiquotation to handle column names passed as strings
+  weight_col <- sym(weight_column)
+  
+  # Dynamically reference grouping columns
+  group_by_cols <- syms(group_by_columns)
+  
+  # Calculate the weighted mean, sum of weights, and count of observations
+  data %>%
+    group_by(!!!group_by_cols) |>
+    summarize(
+      sum_weights = sum(!!weight_col, na.rm = TRUE),
+      count = n()
+      # Add weighted variance. Potential resources:
+      # https://stats.stackexchange.com/questions/51442/weighted-variance-one-more-time
+      # https://influentialpoints.com/Training/two-sample_t-test-principles-properties-assumptions.htm
+    ) |>
+    select(!!!group_by_cols, count, sum_weights)
+}
 
 #' Calculate the Difference in Means Between Two Datasets
 #'
