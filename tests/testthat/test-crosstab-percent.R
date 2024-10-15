88758f587ccf3b0d372e2fca9ef0afefc30e68d3
Lorae <stojanovic.lorae@gmail.com>
2024-10-15 10:02:46 -0400

    test(crosstab_percent): add another unit test with percent_by set to different column

--- tests/testthat/test-crosstab-percent.R
+++ tests/testthat/test-crosstab-percent.R
@@ -18,13 +18,22 @@ devtools::load_all("../dataduck")
 # Read test data from pre-computed CSVs
 input_tb <- read_csv("tests/test-data/crosstab-mean-inputs.csv")
 
-expected_tb <- tribble(
+expected_byage_tb <- tribble(
   ~AGE_bucket, ~RACE_ETH_bucket, ~weighted_count, ~count, ~percent,
   "r00_49",    "white",          65,              2,      0.1589242054,
   "r00_49",    "black",          116,             2,      0.2836185819,
   "r00_49",    "aapi",           228,             5,      0.5574572127,
   "r50plus",   "black",          106,             3,      0.5170731707,
   "r50plus",   "aian",           99,              2,      0.4829268293
+)
+
+expected_byrace_tb <- tribble(
+  ~AGE_bucket, ~RACE_ETH_bucket, ~weighted_count, ~count, ~percent,
+  "r00_49",    "white",          65,              2,      1,
+  "r00_49",    "black",          116,             2,      0.5225225225,
+  "r00_49",    "aapi",           228,             5,      1,
+  "r50plus",   "black",          106,             3,      0.4774774775,
+  "r50plus",   "aian",           99,              2,      1
 )
 
 # ----- Unit tests ----- #
@@ -48,15 +57,47 @@ test_that("crosstab_mean produces correct weighted mean results on database with
     mutate(percent = round(percent, 6)) |>
     arrange(AGE_bucket, RACE_ETH_bucket)
   
-  expected_tb <- expected_tb |>
+  expected_byage_tb <- expected_byage_tb |>
     mutate(percent = round(percent, 6)) |>
     arrange(AGE_bucket, RACE_ETH_bucket)
   
-  print(expected_tb)
+  print(expected_byage_tb)
   print(output_tb)
   
   # Compare results
-  expect_equal(output_tb, expected_tb)
+  expect_equal(output_tb, expected_byage_tb)
+  
+  dbDisconnect(con, shutdown = TRUE)
+})
+
+test_that("crosstab_mean produces correct weighted mean results on database with every_combo = FALSE", {
+  
+  # Create in-memory DuckDB instance and load test input data
+  con <- dbConnect(duckdb::duckdb(), ":memory:")
+  dbWriteTable(con, "input", input_tb, overwrite = TRUE)
+  
+  # Compute weighted mean using DuckDB table
+  output_tb <- crosstab_percent(
+    data = tbl(con, "input"),
+    weight = "PERWT",
+    group_by = c("AGE_bucket", "RACE_ETH_bucket"),
+    percent_group_by = c("RACE_ETH_bucket")
+  ) |> collect()
+  
+  # Round and arrange output for comparison
+  output_tb <- output_tb |>
+    mutate(percent = round(percent, 6)) |>
+    arrange(AGE_bucket, RACE_ETH_bucket)
+  
+  expected_byrace_tb <- expected_byrace_tb |>
+    mutate(percent = round(percent, 6)) |>
+    arrange(AGE_bucket, RACE_ETH_bucket)
+  
+  print(expected_byrace_tb)
+  print(output_tb)
+  
+  # Compare results
+  expect_equal(output_tb, expected_byrace_tb)
   
   dbDisconnect(con, shutdown = TRUE)
 })
