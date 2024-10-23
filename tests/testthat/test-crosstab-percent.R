57ceca48b528be814ce520bb4a5160327e09a1c9
Lorae <10428186+lorae@users.noreply.github.com>
2024-10-23 16:14:28 -0400

    test(crosstab_percent): formulate failing unit test for standard error

--- tests/testthat/test-crosstab-percent.R
+++ tests/testthat/test-crosstab-percent.R
@@ -19,33 +19,33 @@ devtools::load_all("../dataduck")
 input_tb <- read_csv("tests/test-data/crosstab-mean-inputs.csv")
 
 expected_byage_tb <- tribble(
-  ~AGE_bucket, ~RACE_ETH_bucket, ~weighted_count, ~count, ~percent,
-  "r00_49",    "white",          65,              2,      15.89242054,
-  "r00_49",    "black",          116,             2,      28.36185819,
-  "r00_49",    "aapi",           228,             5,      55.74572127,
-  "r50plus",   "black",          106,             3,      51.70731707,
-  "r50plus",   "aian",           99,              2,      48.29268293
+  ~AGE_bucket, ~RACE_ETH_bucket, ~weighted_count, ~count, ~percent,    ~percent_standard_error,
+  "r00_49",    "white",          65,              2,      15.89242054, 0.144386847,
+  "r00_49",    "black",          116,             2,      28.36185819, 0.301949542,
+  "r00_49",    "aapi",           228,             5,      55.74572127, 0.277838466,
+  "r50plus",   "black",          106,             3,      51.70731707, 0.326961631,
+  "r50plus",   "aian",           99,              2,      48.29268293, 0.326961631
 )
 
 expected_byage_combo_tb <- tribble(
-  ~AGE_bucket, ~RACE_ETH_bucket, ~weighted_count, ~count, ~percent,
-  "r00_49",    "white",          65,              2,      15.89242054,
-  "r00_49",    "black",          116,             2,      28.36185819,
-  "r00_49",    "aapi",           228,             5,      55.74572127,
-  "r00_49",    "aian",           0,               0,      0,
-  "r50plus",   "white",          0,               0,      0,
-  "r50plus",   "black",          106,             3,      51.70731707,
-  "r50plus",   "aapi",           0,               0,      0,
-  "r50plus",   "aian",           99,              2,      48.29268293
+  ~AGE_bucket, ~RACE_ETH_bucket, ~weighted_count, ~count, ~percent,    ~percent_standard_error,
+  "r00_49",    "white",          65,              2,      15.89242054, 0.144386847,
+  "r00_49",    "black",          116,             2,      28.36185819, 0.301949542,
+  "r00_49",    "aapi",           228,             5,      55.74572127, 0.277838466,
+  "r00_49",    "aian",           0,               0,      0,           NA,   
+  "r50plus",   "white",          0,               0,      0,           NA,
+  "r50plus",   "black",          106,             3,      51.70731707, 0.326961631,
+  "r50plus",   "aapi",           0,               0,      0,           NA,
+  "r50plus",   "aian",           99,              2,      48.29268293, 0.326961631
 )
 
 expected_byrace_tb <- tribble(
-  ~AGE_bucket, ~RACE_ETH_bucket, ~weighted_count, ~count, ~percent,
-  "r00_49",    "white",          65,              2,      100,
-  "r00_49",    "black",          116,             2,      52.25225225,
-  "r00_49",    "aapi",           228,             5,      100,
-  "r50plus",   "black",          106,             3,      47.74774775,
-  "r50plus",   "aian",           99,              2,      100
+  ~AGE_bucket, ~RACE_ETH_bucket, ~weighted_count, ~count, ~percent,    ~percent_standard_error,
+  "r00_49",    "white",          65,              2,      100,         0,
+  "r00_49",    "black",          116,             2,      52.25225225, 0.645171611,
+  "r00_49",    "aapi",           228,             5,      100,         0,
+  "r50plus",   "black",          106,             3,      47.74774775, 0.645171611,
+  "r50plus",   "aian",           99,              2,      100,         0
 )
 
 # ----- Unit tests ----- #
@@ -61,18 +61,23 @@ test_that("crosstab_percent produces correct weighted mean results on database w
     data = tbl(con, "input"),
     weight = "PERWT",
     group_by = c("AGE_bucket", "RACE_ETH_bucket"),
-    percent_group_by = c("AGE_bucket")
+    percent_group_by = c("AGE_bucket"),
+    repwts = paste0("REPWTP", sprintf("%d", 1:4))
   ) |> collect()
   
   # Round and arrange output for comparison
   output_tb <- output_tb |>
-    mutate(percent = round(percent, 6)) |>
+    mutate(percent = round(percent, 6),
+           percent_standard_error = round(percent_standard_error, 6)) |>
     arrange(AGE_bucket, RACE_ETH_bucket)
   
   expected_byage_tb <- expected_byage_tb |>
-    mutate(percent = round(percent, 6)) |>
+    mutate(percent = round(percent, 6),
+           percent_standard_error = round(percent_standard_error, 6)) |>
     arrange(AGE_bucket, RACE_ETH_bucket)
   
+  print(output_tb)
+  print(expected_byage_tb)
   # Compare results
   expect_equal(output_tb, expected_byage_tb)
   
@@ -96,11 +101,13 @@ test_that("crosstab_percent produces correct weighted mean results on database w
   
   # Round and arrange output for comparison
   output_tb <- output_tb |>
-    mutate(percent = round(percent, 6)) |>
+    mutate(percent = round(percent, 6),
+           percent_standard_error = round(percent_standard_error, 6)) |>
     arrange(AGE_bucket, RACE_ETH_bucket)
   
   expected_byage_combo_tb <- expected_byage_combo_tb |>
-    mutate(percent = round(percent, 6)) |>
+    mutate(percent = round(percent, 6),
+           percent_standard_error = round(percent_standard_error, 6)) |>
     arrange(AGE_bucket, RACE_ETH_bucket)
   
   # Compare results
@@ -125,11 +132,13 @@ test_that("crosstab_percent produces correct weighted mean results on database w
   
   # Round and arrange output for comparison
   output_tb <- output_tb |>
-    mutate(percent = round(percent, 6)) |>
+    mutate(percent = round(percent, 6),
+           percent_standard_error = round(percent_standard_error, 6)) |>
     arrange(AGE_bucket, RACE_ETH_bucket)
   
   expected_byrace_tb <- expected_byrace_tb |>
-    mutate(percent = round(percent, 6)) |>
+    mutate(percent = round(percent, 6),
+           percent_standard_error = round(percent_standard_error, 6)) |>
     arrange(AGE_bucket, RACE_ETH_bucket)
   
   # Compare results
@@ -150,11 +159,13 @@ test_that("crosstab_percent produces correct weighted mean results on tibble wit
   
   # Round and arrange output for comparison
   output_tb <- output_tb |>
-    mutate(percent = round(percent, 6)) |>
+    mutate(percent = round(percent, 6),
+           percent_standard_error = round(percent_standard_error, 6)) |>
     arrange(AGE_bucket, RACE_ETH_bucket)
   
   expected_byrace_tb <- expected_byrace_tb |>
-    mutate(percent = round(percent, 6)) |>
+    mutate(percent = round(percent, 6),
+           percent_standard_error = round(percent_standard_error, 6)) |>
     arrange(AGE_bucket, RACE_ETH_bucket)
   
   # Compare results
