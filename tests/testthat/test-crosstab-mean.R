7893b8d7e6d07439ec86a348336b0eeffb3b8e2b
Lorae <10428186+lorae@users.noreply.github.com>
2024-11-12 18:37:04 -0500

    test(estimate_with_bootstrap_se): create unit test of function with crosstab_mean_no_se

--- tests/testthat/test-crosstab-mean.R
+++ tests/testthat/test-crosstab-mean.R
@@ -8,6 +8,7 @@ library("tibble")
 library("dplyr")
 library("tidyr")
 library("stringr")
+library("purrr")
 
 # Set working directory to project root
 root <- find_root(is_rstudio_project)
@@ -171,5 +172,37 @@ test_that("crosstab_mean produces correct weighted mean results on tibble with e
   # Compare results
   expect_equal(output_tb, expected_combo_tb)
 
+})
+
+test_that("crosstab_mean with estimate_with_boostrap_se produces correct results on tibble, with `every_combo` set to FALSE.", {
+  
+  # Compute weighted and unweighted counts using DuckDB table
+  output_tb <- estimate_with_bootstrap_se(
+    data = input_tb,
+    f = crosstab_mean_no_se,
+    wt_col = "PERWT",
+    repwt_cols = paste0("REPWTP", sprintf("%d", 1:4)),
+    constant = 4/80,
+    se_cols = c("weighted_mean"),
+    id_cols = c("HHINCOME_bucket", "AGE_bucket", "RACE_ETH_bucket"),
+    group_by = c("HHINCOME_bucket", "AGE_bucket", "RACE_ETH_bucket"),
+    every_combo = FALSE,
+    value = "NUMPREC"
+  )
+  
+  # Round and arrange output for comparison
+  output_tb <- output_tb |>
+    arrange(HHINCOME_bucket, AGE_bucket, RACE_ETH_bucket)
+  
+  expected_tb <- expected_tb |>
+    arrange(HHINCOME_bucket, AGE_bucket, RACE_ETH_bucket) |>
+    # Temp: replace NAs with 0 in expected output
+    mutate(across(everything(), ~ replace_na(.x, 0))) |> 
+    # Temp: rename column name
+    rename(se_weighted_mean = mean_standard_error)
+
+  # Compare results
+  expect_equal(output_tb, expected_tb, tolerance = 1e-5)
+  
 })
 
