40dcb158968dcfdcf4f3820786aebe48cc8a65d6
Lorae <10428186+lorae@users.noreply.github.com>
2024-11-12 14:31:15 -0500

    test(estimate_with_bootstrap_se): add passing unit tests with crosstab_count as bootstrapped function

--- tests/testthat/test-crosstab-count.R
+++ tests/testthat/test-crosstab-count.R
@@ -7,6 +7,7 @@ library("duckdb")
 library("tibble")
 library("dplyr")
 library("tidyr")
+library("purrr")
 
 # Set working directory to project root
 root <- find_root(is_rstudio_project)
@@ -120,6 +121,36 @@ test_that("crosstab_count produces correct count results on tibble, with `every_
 
 })
 
+test_that("crosstab_count_no_se with estimate_with_boostrap_se produces correct count results on tibble, with `every_combo` set to TRUE.", {
+  
+  # Compute weighted and unweighted counts using DuckDB table
+  output_tb <- estimate_with_bootstrap_se(
+    data = input_tb,
+    f = crosstab_count_no_se,
+    wt_col = "PERWT",
+    repwt_cols = paste0("REPWTP", sprintf("%d", 1:4)),
+    constant = 4/80,
+    se_cols = c("weighted_count"),
+    group_by = c("AGE_bucket", "RACE_ETH_bucket"),
+    every_combo = TRUE
+  )
+  
+  # Round and arrange output for comparison
+  output_tb <- output_tb |>
+    arrange(AGE_bucket, RACE_ETH_bucket)
+  
+  expected_combo_tb <- expected_combo_tb |>
+    arrange(AGE_bucket, RACE_ETH_bucket) |>
+    # Temp: replace NAs with 0 in expected output
+    mutate(across(everything(), ~ replace_na(.x, 0))) |> 
+    # Temp: rename column name
+    rename(se_weighted_count = standard_error)
+
+  # Compare results
+  expect_equal(output_tb, expected_combo_tb, tolerance = 1e-5)
+  
+})
+
 test_that("crosstab_count produces correct count results on tibble, with `every_combo` set to FALSE.", {
 
   # Compute weighted and unweighted counts using DuckDB table
@@ -141,4 +172,34 @@ test_that("crosstab_count produces correct count results on tibble, with `every_
   # Compare results
   expect_equal(output_tb, expected_tb, tolerance = 1e-5)
 
+})
+
+test_that("crosstab_count_no_se with estimate_with_boostrap_se produces correct count results on tibble, with `every_combo` set to FALSE.", {
+  
+  # Compute weighted and unweighted counts using DuckDB table
+  output_tb <- estimate_with_bootstrap_se(
+    data = input_tb,
+    f = crosstab_count_no_se,
+    wt_col = "PERWT",
+    repwt_cols = paste0("REPWTP", sprintf("%d", 1:4)),
+    constant = 4/80,
+    se_cols = c("weighted_count"),
+    group_by = c("AGE_bucket", "RACE_ETH_bucket"),
+    every_combo = FALSE
+  )
+  
+  # Round and arrange output for comparison
+  output_tb <- output_tb |>
+    arrange(AGE_bucket, RACE_ETH_bucket)
+  
+  expected_tb <- expected_tb |>
+    arrange(AGE_bucket, RACE_ETH_bucket) |>
+    # Temp: replace NAs with 0 in expected output
+    mutate(across(everything(), ~ replace_na(.x, 0))) |> 
+    # Temp: rename column name
+    rename(se_weighted_count = standard_error)
+  
+  # Compare results
+  expect_equal(output_tb, expected_tb, tolerance = 1e-5)
+  
 })
